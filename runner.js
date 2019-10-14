a04444dcbdfe786e4abb93808ee3d7db0810f050
DESKTOP-0DT05M2\Akulenko <takulenko@tula.co>
2019-10-14 14:01:17 +0300

    add junit report

--- runner.js
+++ runner.js
@@ -9,7 +9,7 @@ createTestCafe('localhost')
         return runner
             .src(['./tests/*test.js'])
             .browsers(['chrome:headless'])
-            .reporter('html-testrail')
+            .reporter('junit')
             /*Running all tests in separate instances*/
             .concurrency(3) 
             .run({skipJsErrors: true,
